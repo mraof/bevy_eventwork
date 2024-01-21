1f32be413f9efb7e509f32c890aa254d41c7d147
Noah <noahshomette@gmail.com>
2024-01-21 00:45:01 -0500

    Add documentation for request response stuff, fix lots of old documentation

--- src/managers/network.rs
+++ src/managers/network.rs
@@ -280,7 +280,7 @@ pub(crate) fn handle_new_incoming_connections<
     }
 }
 
-/// A utility trait on [`App`] to easily register [`ServerMessage`]s
+/// A utility trait on [`App`] to easily register [`NetworkMessage`]s
 pub trait AppNetworkMessage {
     /// Register a server message type
     ///
